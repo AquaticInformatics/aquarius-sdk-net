2e6e33cbe179e1f1f821e26993f10577648e572f
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2016-06-06 20:00:31 -0700

    TSS-26 Added empty unit test project for Aquarius.Client project

---
+++ src/Aquarius.Client.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Aquarius.Client.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: ComVisible(false)]
