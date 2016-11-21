9057aa604713ce42782ee518809ed659e50c7b58
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2016-11-21 12:59:23 -0800

    TSS-80 Fixed the code generation script
    1) Parse the API version from the JSON response
    
    2) Append the version as a AquariusServerVersion readonly property to the generated namespace
    
    This will allow consumers to know the API version against which the generator was run.

--- src/Aquarius.Client/generate_code_from_live_endpoint.sh
+++ src/Aquarius.Client/generate_code_from_live_endpoint.sh
@@ -13,7 +13,7 @@ exit_abort () {
 show_usage() {
 	echo usage: `basename $0` "EndPointNamespace EndPointRelativeUrl [ServerName (default: localhost)] [OutputPath (default: .)]"
 	echo
-	echo Generates an Aquarius NG client service model code file via ServiceStack's built-in /types/<language> generator available on a live REST endpoint
+	echo "Generates an Aquarius NG client service model code file via ServiceStack's built-in /types/<language> generator available on a live REST endpoint"
 	echo
 	exit_abort "$@"
 }
@@ -28,15 +28,25 @@ OutputPath=$4
 [ ! -z "$ServerName" ] || ServerName=localhost
 [ ! -z "$OutputPath" ] || OutputPath=.
 
+GlobalNamespace=Aquarius.Client.ServiceModels.$EndPointName
+
 mkdir -p "$OutputPath" || exit_abort "Can't create OutputPath=$OutputPath"
 echo "Determining AQUARIUS Server version ..."
-ApiVersion=`curl -s http://$ServerName/AQUARIUS/apps/v1/version` || exit_abort "Can't determine AQUAIRUS server version of $ServerName"
+ApiVersionJson=`curl -s http://$ServerName/AQUARIUS/apps/v1/version` || exit_abort "Can't determine AQUARIUS server version of $ServerName"
+ApiVersion=`echo "$ApiVersionJson" | sed -e "s/{\"ApiVersion\":\"//" -e "s/\"}//"`
 
 echo "Generating $OutputFile ..."
 OutputFile=$OutputPath/$EndPointName.cs
-curl -s -o t.t "http://$ServerName/AQUARIUS/$EndPoint/types/csharp?MakePartial=false&MakeVirtual=false&GlobalNamespace=Aquarius.Client.$EndPointName&DefaultNamespaces=System,System.Collections,System.Collections.Generic,System.Runtime.Serialization,ServiceStack,ServiceStack.DataAnnotations,NodaTime" || exit_abort "Can't read endpoint"
-echo "// Generated from: $ApiVersion" > "$OutputFile"
-cat t.t >> "$OutputFile"
-rm t.t
+curl -s -o "$OutputFile" "http://$ServerName/AQUARIUS/$EndPoint/types/csharp?MakePartial=false&MakeVirtual=false&GlobalNamespace=$GlobalNamespace&DefaultNamespaces=System,System.Collections,System.Collections.Generic,System.Runtime.Serialization,ServiceStack,ServiceStack.DataAnnotations,NodaTime" || exit_abort "Can't read endpoint"
+
+# Append the generated version to the code
+echo "namespace $GlobalNamespace" >> "$OutputFile"
+echo "{" >> "$OutputFile"
+echo "    public static class Current" >> "$OutputFile"
+echo "    {" >> "$OutputFile"
+echo "        public static readonly AquariusServerVersion Version = AquariusServerVersion.Create(\"$ApiVersion\");" >> "$OutputFile"
+echo "    }" >> "$OutputFile"
+echo "}" >> "$OutputFile"
 unix2dos "$OutputFile"
+
 echo "Complete."
