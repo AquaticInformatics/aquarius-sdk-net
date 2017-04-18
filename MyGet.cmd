ce8eeb64583db956478a5c1e0f0216087e5e8454
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2017-04-17 21:11:00 -0700

    PF-4 Add surrounding quotes to the myget steps

--- MyGet.cmd
+++ MyGet.cmd
@@ -11,21 +11,21 @@ if not "%PackageVersion%" == "" (
 )
 
 rem Package restore
-call %NuGet% restore src
+call "%NuGet%" restore src
 if not "%errorlevel%"=="0" goto failure
 
 rem Build
-call %MsBuildExe% build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+call "%MsBuildExe%" build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
 rem Tests
 set NUnitRunner=src\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe
-call %NUnitRunner% /config:%config% /framework:net-4.5 src\Aquarius.Client.UnitTests\bin\%config%\Aquarius.Client.UnitTests.dll
+call "%NUnitRunner%" /config:%config% /framework:net-4.5 src\Aquarius.Client.UnitTests\bin\%config%\Aquarius.Client.UnitTests.dll
 if not "%errorlevel%"=="0" goto failure
 
 rem Package create
 mkdir Build
-rem call %NuGet% pack "src\Aquarius.Client\Aquarius.Client.csproj" -symbols -o Build -p Configuration=%config% %version%
+rem call "%NuGet%" pack "src\Aquarius.Client\Aquarius.Client.csproj" -symbols -o Build -p Configuration=%config% %version%
 if not "%errorlevel%"=="0" goto failure
 
 :success
