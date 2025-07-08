69d347336d7821d60a00287ba42554395c883115
BuildRobot-AI <BuildRobot-AI@AquaticInformatics.com>
2025-07-08 16:46:34 -0700

    Auto-update .NET SDK

--- src/Aquarius.Client/TimeSeries/Client/ServiceModels/Acquisition.cs
+++ src/Aquarius.Client/TimeSeries/Client/ServiceModels/Acquisition.cs
@@ -1,5 +1,5 @@
 /* Options:
-Date: 2025-04-07 22:34:54
+Date: 2025-07-08 23:46:33
 Version: 6.02
 Tip: To override a DTO option, remove "//" prefix before updating
 BaseUrl: https://develop-1.dev.aquariusdev.net/AQUARIUS/Acquisition/v2
@@ -724,6 +724,6 @@ namespace Aquarius.TimeSeries.Client.ServiceModels.Acquisition
 {
     public static class Current
     {
-        public static readonly AquariusServerVersion Version = AquariusServerVersion.Create("25.1.61.0");
+        public static readonly AquariusServerVersion Version = AquariusServerVersion.Create("25.2.72.0");
     }
 }
