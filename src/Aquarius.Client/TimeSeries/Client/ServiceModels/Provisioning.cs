b73ef23d59e82cdd8df2c6af5857a427d50766de
BuildRobot-AI <BuildRobot-AI@AquaticInformatics.com>
2025-11-02 17:56:51 -0800

    Auto-update Time-Series .NET SDK

--- src/Aquarius.Client/TimeSeries/Client/ServiceModels/Provisioning.cs
+++ src/Aquarius.Client/TimeSeries/Client/ServiceModels/Provisioning.cs
@@ -1,5 +1,5 @@
 /* Options:
-Date: 2025-10-22 02:07:40
+Date: 2025-11-03 01:56:49
 Version: 6.02
 Tip: To override a DTO option, remove "//" prefix before updating
 BaseUrl: https://develop-1.dev.aquariusdev.net/AQUARIUS/Provisioning/v1
@@ -6726,6 +6726,6 @@ namespace Aquarius.TimeSeries.Client.ServiceModels.Provisioning
 {
     public static class Current
     {
-        public static readonly AquariusServerVersion Version = AquariusServerVersion.Create("25.3.106.0");
+        public static readonly AquariusServerVersion Version = AquariusServerVersion.Create("25.4.10.0");
     }
 }
