92c5255f3a8a639ef7571e0599af43286c182cc2
Arthur <david.arthur@aquaticinformatics.com>
2026-03-04 09:56:45 -0800

    AT-2035: Add unit tests for SetTimeout RequestFilter fix

---
+++ src/Aquarius.Client.UnitTests/TimeSeries/Client/SetTimeoutTests.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Net;
+using Aquarius.TimeSeries.Client;
+using Aquarius.TimeSeries.Client.Helpers;
+using FluentAssertions;
+using NUnit.Framework;
+using ServiceStack;
+
+namespace Aquarius.Client.UnitTests.TimeSeries.Client
+{
+    [TestFixture]
+    public class SetTimeoutTests
+    {
+        private AquariusClient _client;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _client = new AquariusClient(AuthenticationType.Credential)
+            {
+                ServerVersion = AquariusServerVersion.Create("0")
+            };
+        }
+
+        [Test]
+        public void SetTimeout_WithRequestTimeout_SetsTimeoutProperty()
+        {
+            var serviceClient = new JsonServiceClient("http://localhost");
+            var timeout = TimeSpan.FromSeconds(300);
+
+            _client.SetTimeout(serviceClient, timeout, null);
+
+            serviceClient.Timeout.Should().Be(timeout);
+        }
+
+        [Test]
+        public void SetTimeout_WithReadWriteTimeout_SetsReadWriteTimeoutProperty()
+        {
+            var serviceClient = new JsonServiceClient("http://localhost");
+            var readWriteTimeout = TimeSpan.FromSeconds(600);
+
+            _client.SetTimeout(serviceClient, null, readWriteTimeout);
+
+            serviceClient.ReadWriteTimeout.Should().Be(readWriteTimeout);
+        }
+
+        [Test]
+        public void SetTimeout_WithRequestTimeout_SetsRequestFilterThatAppliesToHttpWebRequest()
+        {
+            var serviceClient = new JsonServiceClient("http://localhost");
+            var timeout = TimeSpan.FromSeconds(300);
+
+            _client.SetTimeout(serviceClient, timeout, null);
+
+            serviceClient.RequestFilter.Should().NotBeNull();
+
+            var request = WebRequest.CreateHttp("http://localhost");
+            serviceClient.RequestFilter(request);
+
+            request.Timeout.Should().Be((int)timeout.TotalMilliseconds);
+        }
+
+        [Test]
+        public void SetTimeout_WithReadWriteTimeout_SetsRequestFilterThatAppliesReadWriteToHttpWebRequest()
+        {
+            var serviceClient = new JsonServiceClient("http://localhost");
+            var readWriteTimeout = TimeSpan.FromSeconds(600);
+
+            _client.SetTimeout(serviceClient, null, readWriteTimeout);
+
+            serviceClient.RequestFilter.Should().NotBeNull();
+
+            var request = WebRequest.CreateHttp("http://localhost");
+            serviceClient.RequestFilter(request);
+
+            request.ReadWriteTimeout.Should().Be((int)readWriteTimeout.TotalMilliseconds);
+        }
+
+        [Test]
+        public void SetTimeout_WithBothTimeouts_SetsBothOnHttpWebRequest()
+        {
+            var serviceClient = new JsonServiceClient("http://localhost");
+            var timeout = TimeSpan.FromSeconds(300);
+            var readWriteTimeout = TimeSpan.FromSeconds(600);
+
+            _client.SetTimeout(serviceClient, timeout, readWriteTimeout);
+
+            var request = WebRequest.CreateHttp("http://localhost");
+            serviceClient.RequestFilter(request);
+
+            request.Timeout.Should().Be((int)timeout.TotalMilliseconds);
+            request.ReadWriteTimeout.Should().Be((int)readWriteTimeout.TotalMilliseconds);
+        }
+
+        [Test]
+        public void SetTimeout_PreservesExistingRequestFilter()
+        {
+            var serviceClient = new JsonServiceClient("http://localhost");
+            var existingFilterCalled = false;
+            serviceClient.RequestFilter = _ => existingFilterCalled = true;
+
+            _client.SetTimeout(serviceClient, TimeSpan.FromSeconds(300), null);
+
+            var request = WebRequest.CreateHttp("http://localhost");
+            serviceClient.RequestFilter(request);
+
+            existingFilterCalled.Should().BeTrue();
+        }
+
+        [Test]
+        public void SetTimeout_WithNullTimeouts_DoesNotSetTimeoutOnHttpWebRequest()
+        {
+            var serviceClient = new JsonServiceClient("http://localhost");
+            var defaultTimeout = 100000; // .NET default HttpWebRequest.Timeout
+
+            _client.SetTimeout(serviceClient, null, null);
+
+            var request = WebRequest.CreateHttp("http://localhost");
+            serviceClient.RequestFilter(request);
+
+            request.Timeout.Should().Be(defaultTimeout);
+        }
+
+        [Test]
+        public void SetTimeout_WithNonServiceClientBase_DoesNotThrow()
+        {
+            var mockClient = NSubstitute.Substitute.For<IServiceClient>();
+
+            Assert.DoesNotThrow(() => _client.SetTimeout(mockClient, TimeSpan.FromSeconds(300), null));
+        }
+    }
+}
